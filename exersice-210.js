a20c3881326d4b949df259619a2fee3d9a95ad25
arif <bagusharsono55@gmail.com>
2018-08-26 23:51:20 +0700

    Tugas Minggu Ke 2 No 10

---
+++ exersice-210.js
@@ -0,0 +1,19 @@
+//MINGGU KE 2, TUGAS KE 10
+// 1. 
+function bandingkanAngka(angka1, angka2) {
+    if ( angka1===angka2){
+        return -1
+    }
+    if ( angka2>angka1){
+        return true 
+    }else{
+        return false
+    }
+  }
+  
+  // TEST CASES
+  console.log(bandingkanAngka(5, 8)); // true
+  console.log(bandingkanAngka(5, 3)); // false
+  console.log(bandingkanAngka(4, 4)); // -1
+  console.log(bandingkanAngka(3, 3)); // -1
+  console.log(bandingkanAngka(17, 2)); // false
