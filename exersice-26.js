73308b8c9550abc81faa50a8cd5a5aec619e5d56
Bagus181013 <41609643+Bagus181013@users.noreply.github.com>
2018-10-11 20:55:07 +0700

    Update exersice-26.js

--- exersice-26.js
+++ exersice-26.js
@@ -30,18 +30,9 @@ for ( var i = 0; i< 100; i++){
 }
 console.log()
 
-function counter1(){
-    for(var i = 1; i <= 100; i++){
-        if(i % 2 == 0){
-            console.log(i + ' - Genap');
-        }else{
-            console.log(i + ' - Ganjil');
-        }
-    }
-}
 
 // No.3
-function counter2(){
+function counter1(){
     for(var i = 1; i <= 100; i=i+2){
         if(i % 3 == 0){
             console.log(i + ' Kelipatan 3');
@@ -50,7 +41,7 @@ function counter2(){
 }
 
 //No.3
-function counter5(){
+function counter2(){
     for(var i = 1; i <= 100; i=i+5){
         if(i % 6 == 0){
             console.log(i + ' Kelipatan 6');
@@ -60,7 +51,7 @@ function counter5(){
 
 
 //No.3
-function counter9(){
+function counter3(){
     for(var i = 1; i <= 100; i=i+9){
         if(i % 10 == 0){
             console.log(i + ' Kelipatan 10');
@@ -77,7 +68,4 @@ counter1();
 br();
 counter2();
 br();
-counter5()
-br();
-counter9();
-br();
+counter3()
